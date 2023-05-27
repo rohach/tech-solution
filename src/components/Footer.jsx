b6b0abaff4cdd4b7491296afcd72c2b7b8461265
rohanc01 <rohanchaulagain01@gmail.com>
2023-05-27 11:43:27 +0545

    final one

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Tech-Solution</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.facebook.com/rohan.chaulagain.5"
+            target={"blank"}
+          >
+            Facebook
+          </a>
+          <a href="https://www.instagram.com/rohanchaulagain/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/rohach" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
